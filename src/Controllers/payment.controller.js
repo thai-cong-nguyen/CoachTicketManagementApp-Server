f81db6985d64015442f2d2b9682fd33e840c707a
Thai Cong Nguyen <21520169@gm.uit.edu.vn>
2023-12-08 22:06:37 +0700

    Adding Confirm Payment Route

--- src/Controllers/payment.controller.js
+++ src/Controllers/payment.controller.js
@@ -1,8 +1,21 @@
-const { paymentGateway } = require("../Services/payment.service");
+const {
+  paymentGateway,
+  confirmPayment,
+} = require("../Services/payment.service");
 
 exports.paymentGatewayController = async (req, res, next) => {
   try {
     const response = await paymentGateway(req);
+    return res.status(response.code).send(response);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(400).send("Something went wrong");
+  }
+};
+
+exports.confirmPaymentController = async (req, res, next) => {
+  try {
+    const response = await confirmPayment(req);
     return res.status(response.code).send(response);
   } catch (error) {
     console.error(error.message);
